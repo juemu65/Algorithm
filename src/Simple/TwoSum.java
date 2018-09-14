504bdce7286b340c2171677d1d120e9c2613a6e3
juemu65 <840933110@qq.com>
2018-09-14 14:52:09 +0800

    two-sum
    two-sum

--- src/Simple/TwoSum.java
+++ src/Simple/TwoSum.java
@@ -6,16 +6,20 @@ public class TwoSum {
 
     public static int[] twoSum(int[] numbers, int target) {
         HashMap<Integer,Integer> map = new HashMap<>();
-
+        //HashMap
         for (int i = 0; i < numbers.length; i++) {
             if (map.get(numbers[i]) != null) {
-                int[] result = {map.get(numbers[i]), i};
+                int[] result = {map.get(numbers[i]), i+1};
                 return result;
             }
-            map.put(target - numbers[i], i);
+            map.put(target - numbers[i], i+1);
         }
 
         int[] result = {};
         return result;
+
+
     }
+
+
 }
--- src/Simple/on.java
+++ src/Simple/on.java
@@ -6,8 +6,11 @@ import static Simple.TwoSum.twoSum;
 public class on {
     public static void main(String[] args) {
         //two sum
-        int [] sum1={2,7,8,15};
-        twoSum(sum1,9);
+        int[] sum1 = {1,3, 7,6,2, 8, 15};
+        int[] result = twoSum(sum1, 9);
+        for(Integer i:result){
+            System.out.print(i+" ");
+        }
 
     }
 
