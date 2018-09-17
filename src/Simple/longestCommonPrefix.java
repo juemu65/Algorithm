5ce3bc98c58c7df4acac3dd18214e94066fe3627
juemu65 <840933110@qq.com>
2018-09-17 15:58:47 +0800

    longestCommonP
    longestCommonP

---
+++ src/Simple/longestCommonPrefix.java
@@ -0,0 +1,23 @@
+package Simple;
+
+public class longestCommonPrefix {
+
+    public String longestCommonP(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for(int i = 1; i < strs.length; i++) {
+            int j = 0;
+            while( j < strs[i].length() && j < prefix.length() && strs[i].charAt(j) == prefix.charAt(j)) {
+                j++;
+            }
+            if( j == 0) {
+                return "";
+            }
+            prefix = prefix.substring(0, j);
+        }
+        return prefix;
+
+    }
+}
