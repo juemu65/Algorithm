d82276f968cfa09d68d217fe857fd6755956edb5
juemu65 <840933110@qq.com>
2018-10-31 13:48:47 +0800

    LinkCyc
    determine if it has a cycle in it

---
+++ src/Simple/Linkcyc.java
@@ -0,0 +1,29 @@
+package Simple;
+
+public class Linkcyc {
+    //检测环形链表
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+    public Boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode fast, slow;
+        fast = head.next;
+        slow = head;
+        while (fast != slow) {
+            if(fast==null || fast.next==null)
+                return false;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
