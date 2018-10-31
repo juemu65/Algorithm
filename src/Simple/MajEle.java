02398dedb78dec670da13048b5aff5ca7b819d79
juemu65 <840933110@qq.com>
2018-10-31 14:32:48 +0800

    MajorEle

---
+++ src/Simple/MajEle.java
@@ -0,0 +1,27 @@
+package Simple;
+
+
+import java.util.*;
+
+public class MajEle {
+    //求众数
+    public int majorityNumber(List<Integer> nums) {
+
+        int currentMajor = 0;
+        int count = 0;
+
+        for(Integer num : nums) {
+            if(count == 0) {
+                currentMajor = num;
+            }
+
+            if(num == currentMajor) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return currentMajor;
+    }
+}
