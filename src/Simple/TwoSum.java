bff6e9f16d2d32b22de8d84ca2b83dbebcf67b9b
juemu65 <840933110@qq.com>
2018-11-01 13:49:05 +0800

    u
    u

--- src/Simple/TwoSum.java
+++ src/Simple/TwoSum.java
@@ -3,12 +3,19 @@ package Simple;
 import java.util.HashMap;
 
 public class TwoSum {
-    /*
-     *两数之和
-     *
-     *给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
-     *
-     * */
+    //1.两数之和
+    //
+    //给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+
+    //暴力法
+//    for (int i = 0; i < nums.length; i++) {
+//        for (int j = i + 1; j < nums.length; j++) {
+//            if (nums[j] == target - nums[i]) {
+//                return new int[] { i, j };
+//            }
+//        }
+
+
     public static int[] twoSum(int[] numbers, int target) {
         HashMap<Integer,Integer> map = new HashMap<>();
         //HashMap
@@ -25,22 +32,12 @@ public class TwoSum {
 
 
     }
-/*
-暴力法
-    for (int i = 0; i < nums.length; i++) {
-        for (int j = i + 1; j < nums.length; j++) {
-            if (nums[j] == target - nums[i]) {
-                return new int[] { i, j };
-            }
-        }
 
 
 
-
-*/
 }
 
-//两个有序数组的求和
+// 167.两个有序数组的求和
 //双指针
 
 //public int[] twoSum(int[] nums, int target) {
