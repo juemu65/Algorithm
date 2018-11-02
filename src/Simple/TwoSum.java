8286644959b82a00999a9a7c2ca30c86ba57cfdb
juemu65 <840933110@qq.com>
2018-11-02 14:37:09 +0800

    TwoSum
    TwoSum

--- src/Simple/TwoSum.java
+++ src/Simple/TwoSum.java
@@ -6,14 +6,7 @@ public class TwoSum {
     //1.两数之和
     //
     //给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
-
-    //暴力法
-//    for (int i = 0; i < nums.length; i++) {
-//        for (int j = i + 1; j < nums.length; j++) {
-//            if (nums[j] == target - nums[i]) {
-//                return new int[] { i, j };
-//            }
-//        }
+    
 
 
     public static int[] twoSum(int[] numbers, int target) {
