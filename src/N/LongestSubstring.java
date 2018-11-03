e00adeb26b36181e878ed51445b5606c789e4063
juemu65 <840933110@qq.com>
2018-11-03 10:16:33 +0800

    lengthOfLongestSubstring
    lengthOfLongestSubstring

---
+++ src/N/LongestSubstring.java
@@ -0,0 +1,18 @@
+package N;
+
+public class LongestSubstring {
+    //最长无重复字符的子串
+    //给定一个字符串，请找出其中无重复字符的最长子字符串。
+
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+}
