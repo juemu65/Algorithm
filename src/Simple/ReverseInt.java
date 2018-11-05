88ff7709111ea0b8615fa512ccee34ccc92ea67a
juemu65 <840933110@qq.com>
2018-11-05 09:29:54 +0800

    reverse
    op

--- src/Simple/ReverseInt.java
+++ src/Simple/ReverseInt.java
@@ -4,18 +4,16 @@ public class ReverseInt {
 //7.反转整数
     //给定一个 32 位有符号整数，将整数中的数字进行反转。
 
-    public int reverseInteger(int n) {
-        int reversed_n = 0;
-
-        while (n != 0) {
-            int temp = reversed_n * 10 + n % 10;
-            n = n / 10;
-            if (temp / 10 != reversed_n) {
-                reversed_n = 0;
-                break;
-            }
-            reversed_n = temp;
-        }
-        return reversed_n;
+    public int reverse(int x) {
+        long tmp = x; // 防止结果溢出
+        long result = 0;
+        while (tmp != 0) {
+            result = result *10 + tmp % 10;
+            tmp = tmp /10; }
+        // 溢出判断
+        if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE)
+        {
+            result =0; }
+        return (int) result;
     }
 }
