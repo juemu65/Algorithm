460d6d5d9d531bd8bbb73f6f39ba115ca71abf51
juemu65 <840933110@qq.com>
2018-11-07 14:35:28 +0800

    fourSum
    fourSum

--- src/N/FourSum.java
+++ src/N/FourSum.java
@@ -11,47 +11,38 @@ public class FourSum {
 
 
 
-    public List<List<Integer>> fourSum(int[] num, int target) {
-        List<List<Integer>> rst = new ArrayList<List<Integer>>();
-        Arrays.sort(num);
-
-        for (int i = 0; i < num.length - 3; i++) {
-            if (i != 0 && num[i] == num[i - 1]) {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        int len=nums.length;
+        if(len<4){return result;}
+        Arrays.sort(nums);
+        for(int i=0;i<len-3;i++){
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-
-            for (int j = i + 1; j < num.length - 2; j++) {
-                if (j != i + 1 && num[j] == num[j - 1])
+            for(int j=i+1;j<len-2;j++){
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
-
-                int left = j + 1;
-                int right = num.length - 1;
-                while (left < right) {
-                    int sum = num[i] + num[j] + num[left] + num[right];
-                    if (sum < target) {
-                        left++;
-                    } else if (sum > target) {
-                        right--;
-                    } else {
-                        ArrayList<Integer> tmp = new ArrayList<Integer>();
-                        tmp.add(num[i]);
-                        tmp.add(num[j]);
-                        tmp.add(num[left]);
-                        tmp.add(num[right]);
-                        rst.add(tmp);
-                        left++;
-                        right--;
-                        while (left < right && num[left] == num[left - 1]) {
-                            left++;
-                        }
-                        while (left < right && num[right] == num[right + 1]) {
-                            right--;
-                        }
+                }
+                if(nums[i]+nums[j]+nums[j+1]+nums[j+2]>target) break;
+                if(nums[i]+nums[j]+nums[len-2]+nums[len-1]<target) continue;
+                int low=j+1;
+                int high=len-1;
+                while(low<high){
+                    if(nums[i]+nums[j]+nums[low]+nums[high]==target){
+                        result.add(Arrays.asList(nums[i],nums[j],nums[low],nums[high]));
+                        while(low<high&&nums[high]==nums[high-1]){high--;}
+                        while(low<high&&nums[low]==nums[low+1]){low++;}
+                        low++;
+                        high--;
+                    }else if(nums[i]+nums[j]+nums[low]+nums[high]>target){
+                        high--;
+                    }else if(nums[i]+nums[j]+nums[low]+nums[high]<target){
+                        low++;
                     }
                 }
             }
         }
-
-        return rst;
+        return result;
     }
 }
