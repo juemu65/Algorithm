8cc251571ba9c8a563024a5f67e5455a575fbaf1
juemu65 <840933110@qq.com>
2018-11-09 13:49:50 +0800

    maxDepth
    maxDepth

--- src/Simple/MaxDepTree.java
+++ src/Simple/MaxDepTree.java
@@ -4,34 +4,21 @@ public class MaxDepTree {
     //104.二叉树最大深度
 
     public class TreeNode {
-        public int val;
-        public TreeNode left, right;
-        public TreeNode(int val) {
-            this.val = val;
-            this.left = this.right = null;
-        }
+        int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
     }
 
-    private int depth;
 
     public int maxDepth(TreeNode root) {
-        depth = 0;
-        helper(root, 1);
-
-        return depth;
-    }
-
-    private void helper(TreeNode node, int curtDepth) {
-        if (node == null) {
-            return;
+        if (root == null) {
+            return 0;
         }
 
-        if (curtDepth > depth) {
-            depth = curtDepth;
-        }
-
-        helper(node.left, curtDepth + 1);
-        helper(node.right, curtDepth + 1);
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
     }
 }
 
