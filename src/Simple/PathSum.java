990f2c47caf08a5894d7918f2992185ae2f01f05
juemu65 <840933110@qq.com>
2018-11-09 14:36:17 +0800

    hasPathSum
    hasPathSum

--- src/Simple/PathSum.java
+++ src/Simple/PathSum.java
@@ -2,15 +2,26 @@ package Simple;
 
 public class PathSum {
     //112.路径总和
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
 
     public boolean hasPathSum(TreeNode root, int sum) {
-        if (root == null) {
+        if(root==null){
             return false;
         }
-        if (root.left == null && root.right == null) {
-            return sum == root.val;
+        return decide(root,sum);
+    }
+    public boolean decide(TreeNode root, int sum){
+        if(root==null){
+            return false;
         }
-        return hasPathSum (root.left, sum - root.val)
-                || hasPathSum(root.right, sum - root.val);
+        if(root.left==null&&root.right==null&&root.val==sum){
+            return true;
+        }
+        return decide(root.left,sum-root.val)|| decide(root.right,sum-root.val);
     }
 }
