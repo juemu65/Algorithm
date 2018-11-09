f99e22673ba60b12a2c675b3d28290e46634f962
juemu65 <840933110@qq.com>
2018-11-09 13:47:22 +0800

    searchInsert
    searchInsert

--- src/Simple/InsertPos.java
+++ src/Simple/InsertPos.java
@@ -5,36 +5,36 @@ public class InsertPos {
     //给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
     // 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
 
-    public int searchInsert(int[] A, int target) {
-        if (A == null || A.length == 0) {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
         int start = 0;
-        int end = A.length - 1;
+        int end = nums.length - 1;
         int mid;
 
-        if (target < A[0]) {
+        if (target < nums[0]) {
             return 0;
         }
         // find the last number less than target
         while (start + 1 < end) {
             mid = start + (end - start) / 2;
-            if (A[mid] == target) {
+            if (nums[mid] == target) {
                 return mid;
-            } else if (A[mid] < target) {
+            } else if (nums[mid] < target) {
                 start = mid;
             } else {
                 end = mid;
             }
         }
 
-        if (A[end] == target) {
+        if (nums[end] == target) {
             return end;
         }
-        if (A[end] < target) {
+        if (nums[end] < target) {
             return end + 1;
         }
-        if (A[start] == target) {
+        if (nums[start] == target) {
             return start;
         }
         return start + 1;
