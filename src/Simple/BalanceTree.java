6d74ab7376cc752cfdb9e2b997658214b8fa2b10
juemu65 <840933110@qq.com>
2018-11-09 13:37:20 +0800

    BalanceTree
    BalanceTree

--- src/Simple/BalanceTree.java
+++ src/Simple/BalanceTree.java
@@ -1,41 +1,40 @@
 package Simple;
 
-class ResultType {
-    public boolean isBalanced;
-    public int maxDepth;
-    public ResultType(boolean isBalanced, int maxDepth) {
-        this.isBalanced = isBalanced;
-        this.maxDepth = maxDepth;
-    }
-}
+
 
 public class BalanceTree {
-
-    //110.平衡二叉树
-
     public boolean isBalanced(TreeNode root) {
-        return helper(root).isBalanced;
+        if(height(root)>-1){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean bb(int h_lf,int h_ri)
+    {
+        return h_lf-h_ri>0?h_lf-h_ri<=1:h_ri-h_lf<=1;
+    }
+    public int height(TreeNode t) {
+        if(t == null) {
+            return 0;
+        }
+        int left = height(t.left);
+        if(left == -1) {
+            return -1;
+        }
+        int right = height(t.right);
+        if(right == -1) {
+            return -1;
+        }
+
+        if(!bb(left,right)){
+            return -1;
+        }
+        return max(left,right)+1;
     }
 
-    private ResultType helper(TreeNode root) {
-        if (root == null) {
-            return new ResultType(true, 0);
-        }
-
-        ResultType left = helper(root.left);
-        ResultType right = helper(root.right);
-
-        // subtree not balance
-        if (!left.isBalanced || !right.isBalanced) {
-            return new ResultType(false, -1);
-        }
-
-        // root not balance
-        if (Math.abs(left.maxDepth - right.maxDepth) > 1) {
-            return new ResultType(false, -1);
-        }
-
-        return new ResultType(true, Math.max(left.maxDepth, right.maxDepth) + 1);
+    public int max(int a,int b){
+        return a>b?a:b;
     }
 }
 
