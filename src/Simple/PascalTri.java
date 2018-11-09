44a01e47881b802118b469e3857519306ea48876
juemu65 <840933110@qq.com>
2018-11-09 14:21:27 +0800

    generate1&2
    generate1&2

--- src/Simple/PascalTri.java
+++ src/Simple/PascalTri.java
@@ -3,56 +3,57 @@ import java.util.*;
 public class PascalTri {
     //118.杨辉三角
 
-    public ArrayList<ArrayList<Integer>> generate(int numRows) {
-        ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
-        if (numRows == 0) {
-            return rst;
-        }
+    List<List<Integer>> result;
 
-        ArrayList<Integer> first = new ArrayList<Integer>();
-        first.add(0, 1);
-        rst.add(first);
-
-        for (int i = 1; i < numRows; i++) {
-            ArrayList<Integer> tmp = new ArrayList<Integer>(i + 1);
-            for (int j = 0; j < i + 1; j++){
-                tmp.add(-1);
-            }
-            ArrayList<Integer> prev = rst.get(i - 1);
-            tmp.set(0, prev.get(0));
-            tmp.set(i, prev.get(i - 1));
-            for (int j = 1; j < i; j++){
-                tmp.set(j, prev.get(j - 1)+prev.get(j));
-            }
-            rst.add(tmp);
+    public List<List<Integer>> generate(int numRows) {
+        result = new ArrayList<>();
+        if(numRows <= 0){
+            return result;
         }
-        return rst;
+        List<Integer> record = new ArrayList<>();
+        record.add(1);
+        helper(numRows - 1, record);
+        return result;
+    }
+
+    private void helper(int count, List<Integer> record){
+        result.add(new ArrayList<Integer>(record));
+        if(count == 0){
+            return;
+        }
+        List<Integer> list = new ArrayList<>();
+        int num = 0;
+        record.add(0);
+        for(int i = 0; i < record.size(); i++){
+            int sum = num + record.get(i);
+            list.add(sum);
+            num = record.get(i);
+        }
+        helper(count - 1, list);
     }
 
     // 119.杨辉三角II
 
     // 给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行
-    //
-    // public ArrayList<Integer> getRow(int rowIndex) {
-    //        ArrayList<Integer> rst = new ArrayList<Integer>();
-    //        rowIndex += 1;
-    //        if (rowIndex == 0) {
-    //            return rst;
-    //        }
-    //
-    //        rst.add(1);
-    //        for (int i = 1; i < rowIndex; i++) {
-    //            ArrayList<Integer> tmp = new ArrayList<Integer>(i+1);
-    //            for (int j = 0; j < i + 1; j++) {
-    //                tmp.add(-1);
-    //            }
-    //            tmp.set(0, rst.get(0));
-    //            tmp.set(i, rst.get(i - 1));
-    //            for (int j = 1; j < i; j++) {
-    //                tmp.set(j, rst.get(j - 1) + rst.get(j));
-    //            }
-    //            rst = tmp;
-    //        }
-    //        return rst;
-    //    }
+    /*
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new ArrayList<Integer>();
+        if (rowIndex < 0)
+            return list;
+
+        for (int i = 0;i <= rowIndex;i++) {
+            list.add(0);
+        }
+
+        list.set(0, 1);
+        for (int i = 1;i <= rowIndex;i++) {
+            for (int j = i;j >= 0;j--) {
+                list.set(j, (j - 1 < 0? 0 : list.get(j - 1)) + list.get(j));
+            }
+        }
+
+        return list;
+    }
+
+     */
 }
