84383e55bb5d240c89a3eda201313eb8cc62bf49
juemu65 <840933110@qq.com>
2018-11-09 15:06:18 +0800

    removeDuplicates
    removeDuplicates

--- src/Simple/SortedArr.java
+++ src/Simple/SortedArr.java
@@ -6,15 +6,15 @@ public class SortedArr {
     //
     //不要使用额外的数组空间，必须在原地没有额外空间的条件下完成。
 
-    public int removeDuplicates(int[] A) {
-        if (A == null || A.length == 0) {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
 
         int size = 0;
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] != A[size]) {
-                A[++size] = A[i];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[size]) {
+                nums[++size] = nums[i];
             }
         }
         return size + 1;
@@ -23,23 +23,21 @@ public class SortedArr {
     /*
     //80.排序链表中重复元素
     //允许出现两次
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
     public int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int index = 0, count = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[index]) {
-                if (count < 2) {
-                    nums[++index] = nums[i];
-                    count ++;
-                }
-            } else {
-                nums[++index] = nums[i];
-                count = 1;
+         int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count < 2 || nums[i] > nums[count - 2]) {
+                nums[count++] = nums[i];
             }
         }
-        return index + 1;
+        return count;
     }
 
 
