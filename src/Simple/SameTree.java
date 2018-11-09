e99318ecf43961f718160ae6f3097862ee96f438
juemu65 <840933110@qq.com>
2018-11-09 14:54:03 +0800

    isIdentical
    SameTree

--- src/Simple/SameTree.java
+++ src/Simple/SameTree.java
@@ -7,29 +7,28 @@ public class SameTree {
     //检查两棵二叉树是否等价。
     // 等价的意思是说，首先两棵二叉树必须拥有相同的结构，
     // 并且每个对应位置上的节点上的数都相等。
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
 
-
-    public boolean isIdentical(TreeNode a, TreeNode b) {
-        // write your code here
-
-        if (a == null && b == null) {
+    public boolean isIdentical(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
             return true;
         }
 
-        if (a == null || b == null) {
+        if (p == null || q == null) {
             return false;
         }
 
-        if (a.val != b.val) {
-
+        if (p.val != q.val) {
+            //这里不能写成a != b， 因为a和b分别是两棵树的根节点，显然不同。
+            //这里要比较的是a和b的值是否相同，如果数值不同，直接返回false, 如果相同，再看他们的左右子树是否一致
             return false;
         }
-
-        if (a.val == b.val){
-            return isIdentical(a.left, b.left) && isIdentical(a.right, b.right);
-        }else{
-            return false;
-        }
+        return isIdentical(p.left, q.left) && isIdentical(p.right, q.right);
 
     }
 }
