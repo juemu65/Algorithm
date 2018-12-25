7f0e6d777a7134897450adb07ee2b4bb3beb2325
juemu65 <840933110@qq.com>
2018-12-25 09:02:58 +0800

    introduce
    introduce

--- src/Simple/BalanceTree.java
+++ src/Simple/BalanceTree.java
@@ -3,6 +3,20 @@ package Simple;
 
 
 public class BalanceTree {
+
+    //110. 平衡二叉树
+    //给定一个二叉树，判断它是否是高度平衡的二叉树。
+    //
+    //本题中，一棵高度平衡二叉树定义为：
+    //
+    //一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
     public boolean isBalanced(TreeNode root) {
         if(height(root)>-1){
             return true;
